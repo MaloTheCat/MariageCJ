0b3be51cf6c7a5bef2d6e7a5e51a9ac53995e1eb
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-05-20 10:06:33 +0200

    setting up the DB

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'stylesheets/modal'
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import { initCountdown } from '../controllers/countdown';
+// import { initModal } from '../controllers/modal';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 initCountdown();
+// initModal();
