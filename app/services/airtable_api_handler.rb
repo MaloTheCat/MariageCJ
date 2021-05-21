e1da035b200762ea3704dd59a9e4c1bcb84c2915
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-05-21 14:20:16 +0200

    API Airtable and list done

---
+++ app/services/airtable_api_handler.rb
@@ -0,0 +1,24 @@
+require "uri"
+require "net/http"
+require 'json'
+
+class AirtableApiHandler
+  def self.get_all_rows
+    url = URI("https://api.airtable.com/v0/appsnmy0OMuSEkwJS/C&J")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer key1bjpizahHwm1vJ"
+    request["Cookie"] = "brw=brwbfYTmJqaGnv7ZD"
+
+    response = https.request(request)
+    JSON.parse(response.read_body).flatten[1]
+  end
+
+  def self.patch_row
+    # get_all_rows
+  end
+
+end
